1fb0fd47c3b622db07df44003030da490f41f632
zen-ku <kariyakariya399@gmail.com>
2024-01-01 23:49:06 +0100

    trying the htm to image package

--- app/api/handle/route.ts
+++ app/api/handle/route.ts
@@ -1,10 +1,20 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { NextResponse } from 'next/server'
+const nodeHtmlToImage = require('node-html-to-image')
+
 
 type ResponseData = {
 message: string
 }
 
+
 export async function GET() {
+    nodeHtmlToImage({
+        output: './files/image.png',
+        // output: './../../../files/image.png',
+        html: '<html><body>Hello world!</body></html>'
+    })
+    .then(() => console.log('The image was created successfully!'))
+
 return NextResponse.json({ message: 'Hello from Next.js!' })
 }
